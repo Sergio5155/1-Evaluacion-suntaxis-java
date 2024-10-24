e40f1cdb7fd4159ea2d4209d0370771f0215e5a9
Sergio5155 <sergiocasamayor55@gmail.com>
2024-10-24 10:13:57 +0200

    22/10/2024

--- Bingo/src/Main.java
+++ Bingo/src/Main.java
@@ -1,52 +1,48 @@
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int filas = 3;
         int columnas = 9;
-        int[][] matriz = new int[filas][columnas];
-        Random random = new Random();
-        // Rellenar la matriz con números aleatorios en los rangos especificados
+        int matriz[][] = new int[filas][columnas];
+        Random aleatorio = new Random();
+
+        // Llenar la matriz con números aleatorios
         for (int j = 0; j < columnas; j++) {
             int rangoInferior = j * 10;
             for (int i = 0; i < filas; i++) {
-                matriz[i][j] = rangoInferior + random.nextInt(10); // Genera un número aleatorio en el rango
+                matriz[i][j] = rangoInferior + aleatorio.nextInt(10);
             }
         }
-        // Mostrar la matriz original
-        System.out.println("Matriz original:");
-        imprimirMatriz(matriz);
 
-        // Ordenar cada columna de la matriz
-        for (int j = 0; j < columnas; j++) {
-            ordenarColumna(matriz, j);
+        System.out.println("Matriz original:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
         }
 
-        // Mostrar la matriz ordenada
-        System.out.println("\nMatriz ordenada:");
-        imprimirMatriz(matriz);
-    }
-
-    // Método para ordenar una columna específica usando el método de burbuja
-    public static void ordenarColumna(int[][] matriz, int columna) {
-        for (int i = 0; i < matriz.length - 1; i++) {
-            for (int j = 0; j < matriz.length - 1 - i; j++) {
-                if (matriz[j][columna] > matriz[j + 1][columna]) {
-                    // Intercambiar
-                    int temp = matriz[j][columna];
-                    matriz[j][columna] = matriz[j + 1][columna];
-                    matriz[j + 1][columna] = temp;
+        // Ordenar cada columna de menor a mayor
+        for (int j = 0; j < columnas; j++) {
+            for (int i = 0; i < filas - 1; i++) {
+                for (int k = i + 1; k < filas; k++) {
+                    if (matriz[i][j] > matriz[k][j]) {
+                        // Intercambiar los elementos
+                        int temp = matriz[i][j];
+                        matriz[i][j] = matriz[k][j];
+                        matriz[k][j] = temp;
+                    }
                 }
             }
         }
-    }
 
-    // Método para imprimir la matriz
-    public static void imprimirMatriz(int[][] matriz) {
-        for (int[] fila : matriz) {
-            for (int num : fila) {
-                System.out.print(num + "\t");
+        System.out.println("\nMatriz ordenada por columnas:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
