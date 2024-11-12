a617b52576436a8ff76ebf5239cf7d80e6727103
Sergio5155 <sergiocasamayor55@gmail.com>
2024-11-12 11:33:28 +0100

    12/11/2024

--- Jeroglificos/src/Main.java
+++ Jeroglificos/src/Main.java
@@ -1,40 +1,23 @@
 import java.util.Scanner;
-
 public class Main {
-
-    // Mapeo de símbolos jeroglíficos a las potencias de 10
-    private static final char[] SIMBOLOS = {'H', 'R', 'D', 'F', 'C', 'G', 'T'};
-    private static final int[] VALORES = {1000000, 100000, 10000, 1000, 100, 10, 1};
-
-    // Método que convierte un número en su representación jeroglífica
-    public static String convertirAJeroglifico(int numero) {
-        StringBuilder resultado = new StringBuilder();
-
-        for (int i = 0; i < VALORES.length; i++) {
-            int cantidad = numero / VALORES[i];
-            numero %= VALORES[i];
-
-            for (int j = 0; j < cantidad; j++) {
-                resultado.append(SIMBOLOS[i]);
-            }
-        }
-
-        return resultado.toString();
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        char[] simbolos = {'T', 'G', 'C', 'F', 'D', 'R', 'H'};
+        int[] potencias = {1, 10, 100, 1000, 10000, 100000, 1000000};
         while (true) {
-            int numero = scanner.nextInt();
-
-            if (numero == 0) {
-                break; // Fin de entrada
+            int numero = sc.nextInt();
+            if (numero == 0) break;
+            String resultado = "";
+            int tamanyo = String.valueOf(numero).length(); // LONGITUD
+            for (int i = (tamanyo-1); i >= 0; i--) {
+                int digito = numero / potencias[i];
+                numero %= potencias[i];
+                for (int j = 0; j < digito; j++) {
+                    resultado = resultado + simbolos[i];
+                }
             }
-
-            System.out.println(convertirAJeroglifico(numero));
+            System.out.println(resultado);
         }
-
-        scanner.close();
+        sc.close();
     }
 }
